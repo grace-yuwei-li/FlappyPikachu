8f7a9609334cd751e5dc5b3585d5a6c9556d4aa5
grace <grace.yuwei.li@gmail.com>
2021-06-02 14:39:41 -0400

    Initial commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
+import 'package:flappy_pikachu/bird.dart';
 import 'package:flutter/material.dart';
+
+import 'barriers.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,20 +11,155 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static double pikachuYaxis = 0;
+  double time = 0;
+  double height = 0;
+  double initialHeight = pikachuYaxis;
+  bool gameHasStarted = false;
+  static double barrierXone = 1;
+  double barrierXtwo = barrierXone + 1.5;
+
+  void jump() {
+    setState(() {
+      time = 0;
+      initialHeight = pikachuYaxis;
+    });
+  }
+
+  void startGame() {
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 60), (timer) {
+      time += 0.05;
+      height = -4.9 * time * time + 2.8 * time;
+      setState(() {
+        pikachuYaxis = initialHeight - height;
+      });
+
+      setState(() {
+        if (barrierXone < -2) {
+          barrierXone += 3.6;
+        } else {
+          barrierXone -= 0.05;
+        }
+      });
+
+      setState(() {
+        if (barrierXtwo < -2) {
+          barrierXtwo += 3.6;
+        } else {
+          barrierXtwo -= 0.05;
+        }
+      });
+
+      if (pikachuYaxis > 1) {
+        timer.cancel();
+        gameHasStarted = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      children: [
-        Expanded(
-          flex: 2,
-          child: Container(color: Colors.blue),
-        ),
-        Expanded(
-            child: Container(
-          color: Colors.green,
-        ))
-      ],
-    ));
+    return GestureDetector(
+        onTap: () {
+          if (gameHasStarted) {
+            jump();
+          } else {
+            startGame();
+          }
+        },
+        child: Scaffold(
+            body: Column(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Stack(
+                  children: [
+                    AnimatedContainer(
+                      alignment: Alignment(0, pikachuYaxis),
+                      duration: Duration(milliseconds: 0),
+                      color: Colors.blue,
+                      child: MyBird(),
+                    ),
+                    Container(
+                      alignment: Alignment(0, -0.3),
+                      child: gameHasStarted
+                          ? Text(" ")
+                          : Text("T A P  T O  P L A Y",
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.white)),
+                    ),
+                    AnimatedContainer(
+                      alignment: Alignment(barrierXone, 1.1),
+                      duration: Duration(milliseconds: 0),
+                      child: MyBarrier(
+                        size: 150.0,
+                      ),
+                    ),
+                    AnimatedContainer(
+                      alignment: Alignment(barrierXone, -1.1),
+                      duration: Duration(milliseconds: 0),
+                      child: MyBarrier(
+                        size: 150.0,
+                      ),
+                    ),
+                    AnimatedContainer(
+                      alignment: Alignment(barrierXtwo, 1.1),
+                      duration: Duration(milliseconds: 0),
+                      child: MyBarrier(
+                        size: 100.0,
+                      ),
+                    ),
+                    AnimatedContainer(
+                      alignment: Alignment(barrierXtwo, -1.1),
+                      duration: Duration(milliseconds: 0),
+                      child: MyBarrier(
+                        size: 200.0,
+                      ),
+                    )
+                  ],
+                )),
+            Container(
+              height: 15,
+              color: Colors.green,
+            ),
+            Expanded(
+                child: Container(
+                    color: Colors.brown,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("SCORE",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20)),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text("0",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 35))
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("BEST",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20)),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text("10",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 35))
+                          ],
+                        ),
+                      ],
+                    )))
+          ],
+        )));
   }
 }
